fc177f36b61dccb5ae70ed22435199f67885111c
important+github@jimalcock.dev <important+github@jimalcock.dev>
2024-07-17 15:36:34 +0000

    initial commit

---
+++ __main__.py
@@ -0,0 +1,20 @@
+"""Template Main File."""
+
+import getpass
+
+
+class Greeting:
+    """Template Greeting Class."""
+
+    def __init__(self) -> None:
+        """Class initialization."""
+        self.greeting = f"Hello {getpass.getuser()}!!!"
+
+    def greet(self) -> str:
+        """Return a greeting string."""
+        return self.greeting
+
+
+if __name__ == "__main__":
+    my_greet = Greeting()
+    print(my_greet.greet())
